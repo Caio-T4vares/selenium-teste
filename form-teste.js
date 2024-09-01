8f09979c8f103fc97896ab33a02ed3d5e3c04446
Caio <102376017+Caio-T4vares@users.noreply.github.com>
2024-09-01 14:24:16 -0300

    teste implementado para formulario de cadastro

---
+++ form-teste.js
@@ -0,0 +1,25 @@
+const selenium = require("selenium-webdriver");
+(
+async function(){
+  const driver = await new selenium.Builder().forBrowser(selenium.Browser.CHROME).build();
+
+  await driver.get('http://localhost:3000/Cadastrar');
+
+  let firstName = await driver.findElement(selenium.By.id("nome"));
+  let surname = await driver.findElement(selenium.By.id("sobrenome"));
+  let cpf = await driver.findElement(selenium.By.id("cpf"));
+  let password = await driver.findElement(selenium.By.id("senha"));
+
+  await firstName.sendKeys("Micagou");
+  await surname.sendKeys("Alves");
+  await cpf.sendKeys("11111111111") 
+  await password.sendKeys("password12342");
+
+  let saveBtn = await driver.findElement(selenium.By.xpath("/html/body/div[1]/div/div[2]/button[2]"));
+
+  await saveBtn.click();
+
+
+
+  await driver.quit();
+})();
